bae99f169f1e221adcaee6788591ca4524b43924
rasmus-veike <rasmus.veike@tptlive.ee>
2024-02-27 00:59:36 +0200

    .

---
+++ bulma-message-demo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Bulma Message Component Demo</h1>
+      <Message type="is-primary" title="Primary Message">
+        This is a primary message using Bulma CSS framework.
+      </Message>
+      <Message type="is-link" title="Link Message">
+        This is a link message using Bulma CSS framework.
+      </Message>
+      <Message type="is-info" title="Info Message">
+        This is an info message using Bulma CSS framework.
+      </Message>
+      <Message type="is-success" title="Success Message">
+        This is a success message using Bulma CSS framework.
+      </Message>
+      <Message type="is-warning" title="Warning Message">
+        This is a warning message using Bulma CSS framework.
+      </Message>
+      <Message type="is-danger" title="Danger Message">
+        This is a danger message using Bulma CSS framework.
+      </Message>
+    </div>
+  </div>
+</template>
+
+<script>
+import Message from './components/MessageBox.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Message
+  }
+};
+</script>
+
+<style>
+.container {
+  margin-top: 20px;
+}
+</style>
---
+++ bulma-message-demo/src/components/MessageBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <article :class="['message', type]">
+    <div class="message-header">
+      <p>{{ title }}</p>
+      <button class="delete" aria-label="delete" @click="$emit('close')"></button>
+    </div>
+    <div class="message-body">
+      <slot></slot>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+/* You can add custom styles here */
+</style>
